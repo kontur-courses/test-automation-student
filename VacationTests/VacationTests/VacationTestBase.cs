145cd0acacf538211bda766a19ea4588d5d9156e
Simonov Mihail <simonov.mn@skbkontur.ru>
2023-02-27 13:47:18 +0500

    Убрал параллельность и пул браузеров

--- VacationTests/VacationTests/VacationTestBase.cs
+++ VacationTests/VacationTests/VacationTestBase.cs
@@ -1,4 +1,3 @@
-using System.Collections.Concurrent;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using VacationTests.Claims;
@@ -6,33 +5,21 @@ using VacationTests.Infrastructure;
 using VacationTests.Infrastructure.PageElements;
 using VacationTests.PageNavigation;
 
+// Классы с тестами запускаются параллельно.
+// Тесты внутри одного класса проходят последовательно.
+[assembly: Parallelizable(ParallelScope.Fixtures)]
+
 namespace VacationTests
 {
-    public class VacationTestBase
+    public abstract class VacationTestBase
     {
-        // Создание словаря для хранения браузеров пула
-        private static readonly ConcurrentDictionary<string, IWebDriver> AcquiredWebDrivers = new();
-
-        // Свойство, которое с помощью NUnit отдаёт текущий WorkerId теста
-        private static string TestWorkerId => TestContext.CurrentContext.WorkerId ?? "debug";
-        protected IWebDriver WebDriver => GetWebDriver();
+        protected IWebDriver WebDriver = new ChromeDriverFactory().Create();
         protected ClaimStorage ClaimStorage => new(LocalStorage);
         protected LocalStorage LocalStorage => new(WebDriver);
         private ControlFactory ControlFactory => new(LocalStorage, ClaimStorage);
         protected Navigation Navigation => new(WebDriver, ControlFactory);
-        
-        // Возвращеия браузера в пул после каждого теста
-        [TearDown]
-        public void TearDown()
-        {
-            if (AcquiredWebDrivers.TryRemove(TestWorkerId, out var webDriver))
-                AssemblySetUpFixture.WebDriverPool.Release(webDriver);
-        }
 
-        // Метод получения текущего WebDriver
-        private IWebDriver GetWebDriver()
-        {
-            return AcquiredWebDrivers.GetOrAdd(TestWorkerId, _ => AssemblySetUpFixture.WebDriverPool.Acquire());
-        }
+        [OneTimeTearDown]
+        protected void OneTimeTearDown() => WebDriver.Dispose();
     }
 }
