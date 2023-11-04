fa0fea8ba27da5a1f03cf2111db7f1c4f8e3522a
Evgenia Lopatina <e.lopatina@skbkontur.ru>
2023-11-05 00:29:12 +0300

    task 5.1

--- VacationTests/VacationTests/Claims/DirectorBuilder.cs
+++ VacationTests/VacationTests/Claims/DirectorBuilder.cs
@@ -2,5 +2,28 @@ namespace VacationTests.Claims
 {
     public class DirectorBuilder
     {
+        private int id = 14;
+        private string name = "Бублик Владимир Кузьмич";
+        private string position = "Директор департамента";
+
+        public DirectorBuilder WithId(int newId)
+        {
+            id = newId;
+            return this;
+        }
+
+        public DirectorBuilder WithName(string newName)
+        {
+            name = newName;
+            return this;
+        }
+
+        public DirectorBuilder WithPosition(string newPosition)
+        {
+            position = newPosition;
+            return this;
+        }
+
+        public Director Build() => new Director(id, name, position);
     }
 }
--- VacationTests/VacationTests/Data/Directors.cs
+++ VacationTests/VacationTests/Data/Directors.cs
@@ -4,10 +4,10 @@ namespace VacationTests.Data
 {
     public static class Directors
     {
-        public static Director Default => new Director(14, "Бублик Владимир Кузьмич", "Директор департамента");
+        public static Director Default => new DirectorBuilder().Build();
 
         public static Director SuperDirector =>
-            new Director(24320, "Кирпичников Алексей Николаевич", "Руководитель управления");
+            new DirectorBuilder().WithId(24320).WithName("Кирпичников Алексей Николаевич").WithPosition("Руководитель управления").Build();
 
         public static DirectorBuilder New => new DirectorBuilder();
     }
