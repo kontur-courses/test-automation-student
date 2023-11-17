7499e285f41a39f14d113e343900e969584cd4a6
Evgenia Lopatina <e.lopatina@skbkontur.ru>
2023-11-17 23:14:27 +0300

    add task 8.2

--- VacationTests/VacationTests/Infrastructure/FirefoxDriverFactory.cs
+++ VacationTests/VacationTests/Infrastructure/FirefoxDriverFactory.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Kontur.Selone.Extensions;
 using Kontur.Selone.WebDrivers;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
@@ -12,9 +11,18 @@ namespace VacationTests.Infrastructure
         {
             var firefoxDriverService = FirefoxDriverService.CreateDefaultService(AppDomain.CurrentDomain.BaseDirectory);
             var options = new FirefoxOptions();
-            options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+            //не уверена, что это аналог --no-sandbox, но это самое близкое, что я нагуглила. И оно НЕ работает :(
+            options.SetPreference("security.sandbox.content.level", "0");
+            //аналогов --disable-extensions я не нашла и как задать размер экрана тоже
+            //поэтому ниже просто вроде бы полезнаце штуки
+            options.SetPreference("browser.download.dir", "C:\\Windows\\temp");
+            options.SetPreference("browser.download.useDownloadDir", true);
+            options.SetPreference("browser.download.viewableInternally.enabledTypes", "");
+            //options.SetPreference("browser.helperApps.neverAsk.saveToDisk", "application/pdf;text/plain;application/text;text/xml;application/xml");
+            options.SetPreference("pdfjs.disabled", true);  // disable the built-in PDF viewer
+            options.BrowserExecutableLocation = @"C:\Program Files\Mozilla Firefox\firefox.exe";
             var firefoxDriver = new FirefoxDriver(firefoxDriverService, options, TimeSpan.FromSeconds(180));
-            firefoxDriver.Manage().Window.SetSize(1280, 960);
+            //firefoxDriver.Manage().Window.SetSize(1280, 560);
             return firefoxDriver;
         }
     }
