c926442210a462d37d644277f9458571afc58174
Evgenia Lopatina <e.lopatina@skbkontur.ru>
2023-11-20 18:54:02 +0300

    task 13

--- VacationTests/VacationTests/Tests/Navigation/NavigationTests.cs
+++ VacationTests/VacationTests/Tests/Navigation/NavigationTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
+using VacationTests.Infrastructure.PageElements;
 
 namespace VacationTests.Tests.Navigation
 {
@@ -13,6 +14,7 @@ namespace VacationTests.Tests.Navigation
             var adminPage = enterPage.LoginAsAdmin();
             adminPage.WaitLoaded();
             adminPage.IsAdminPage.Should().BeTrue();
+            adminPage.ClaimList.WaitAbsence();
         }
     }
 }
