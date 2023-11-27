71b730a89bea0c6908c08992c842e407a06398c5
taranovakn <taranova.kn@skbkontur.ru>
2023-11-27 21:17:03 +0300

    Add category [flaky]

--- VacationTests/VacationTests/Tests/Navigation/NavidationTests.cs
+++ VacationTests/VacationTests/Tests/Navigation/NavidationTests.cs
@@ -1,9 +1,11 @@
 ﻿using NUnit.Framework;
+using VacationTests.Infrastructure.PageElements;
 
 namespace VacationTests.Tests.Navigation
 {
     public class NavigationTests : VacationTestBase
     {
+        [Category("Flaky")]
         [Test]
         public void AdminButton_PassToAdminPage_Success()
         {
@@ -13,6 +15,7 @@ namespace VacationTests.Tests.Navigation
             var adminPage = enterPage.LoginAsAdmin();//Act
 
             Assert.IsTrue(adminPage.IsAdminPage); //Assert
+            adminPage.ClaimList.WaitAbsence();
         }
     }
 }
